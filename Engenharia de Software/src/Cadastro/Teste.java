83f4c6f6db26ac1590381cb7d4821de3cdc36f65
Tobias Sousa <100934496+tobiassousa@users.noreply.github.com>
2022-11-25 17:11:10 -0300

    Teste Realizado

---
+++ Engenharia de Software/src/Cadastro/Teste.java
@@ -0,0 +1,33 @@
+package Cadastro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void test() {
+		
+		Garagem garagem = new Garagem ();
+		
+		assertEquals(garagem.getOnibus().size(),0);
+		
+		garagem.cadastraronibus(new Onibus("ABC1111", new Especificacao("ABC1111", "20", "Putim1", "3011", "180", "variant")));
+		garagem.cadastraronibus(new Onibus("ABC2222", new Especificacao("ABC2222", "20", "Putim", "3015", "180", "variant")));
+		
+		assertEquals(garagem.getOnibus().size(),2);
+		
+		Onibus busEncontrado = garagem.buscaronibusPorPlaca("ABC2222");
+		
+		assertEquals(busEncontrado.getEspec().getLinha(), "Putim1");
+		
+		List<Onibus> busEncontrados = garagem.buscarOnibusPorEspecificacao(new Especificacao("ABC2222", "20", "Putim", "3015", "180", "variant"));
+		
+		assertEquals(busEncontrados.get(0).getEspec().getLinha(),"Putim");
+		
+	}
+
+}
